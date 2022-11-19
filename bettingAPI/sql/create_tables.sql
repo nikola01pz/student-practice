ca8b90a45bb21f6e97589a6391ad562cac6379d4
Nikola Istvanic <nikola01pz@gmail.com>
2022-11-19 15:10:14 +0100

    update database structure and insert data in tables

--- bettingAPI/sql/create_tables.sql
+++ bettingAPI/sql/create_tables.sql
@@ -1,29 +1,28 @@
-create table league(
-	IDleague smallint primary key,  -- starts with 0xxx
-	title varchar(30),
-    offer smallint
+create table leagues(
+	league_id int primary key,  -- starts with 1xxxx
+	title varchar(30)
 );
 
-create table offer(
-	IDoffer smallint primary key, -- starts with 1xxx
+create table offers(
+	offer_id int primary key, -- starts with 2xxxx
     game varchar(40),
     sport varchar(15),
-    tip smallint,
-    time_played datetime,
-    tv_channel varchar(10),
-	constraint fkTip foreign key(tip) references tip(IDtip)
-);
-
-create table tip(
-	IDtip smallint primary key, -- starts with 2xxx
-    tip varchar(25),
-	coefficient smallint
+    time_played varchar(25),
+    tv_channel varchar(10)
 );
 
 create table league_offers( -- relation n:m between league and offer
-	IDleague smallint not null,
-    IDoffer smallint not null,
-    constraint fkLeague foreign key (IDleague) references league(IDleague),
-    constraint fkOffer foreign key (IDoffer) references offer(IDoffer),
-    constraint pkOffers primary key(IDleague, IDoffer)
+	league_id int not null,
+    offer_id int not null,
+    constraint fk_league_offers_leagues foreign key (league_id) references leagues(league_id),
+    constraint fk_league_offers_offers foreign key (offer_id) references offers(offer_id),
+    constraint pk_league_offers primary key(league_id, offer_id)
+);
+
+create table offer_tips( -- starts with 3xxxx
+	offer_id int not null,
+    tip varchar(25) not null,
+    columne decimal(4,2),
+    constraint fk_offer_tips_offers foreign key (offer_id) references offers(offer_id),
+    constraint pk_offer_tip primary key(offer_id, tip)
 );

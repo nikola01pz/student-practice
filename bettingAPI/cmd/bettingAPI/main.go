485f2c5154b734ecc88fc24519a26983c3f9aff6
Nikola Istvanic <nikola01pz@gmail.com>
2022-12-02 14:45:23 +0100

    fix multiple connections to database

--- bettingAPI/cmd/bettingAPI/main.go
+++ bettingAPI/cmd/bettingAPI/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	bethttp "bettingAPI/internal/http"
 	"bettingAPI/internal/mysql"
-	"database/sql"
-	"fmt"
+	"bettingAPI/internal/source"
 	"log"
 	"net/http"
 
@@ -13,61 +12,16 @@ import (
 )
 
 func main() {
-	// db := mysql.NewDB()
-	// hdl := NewHandler(db)
+	db := mysql.NewDB()
+	hdl := bethttp.NewHandler(db)
 
-	GetAllOffersFromHTTP()
-	GetAllLeaguesFromHTTP()
+	offers := source.GetAllOffersFromSource()
+	db.InsertOffers(offers)
+	db.InsertTips(offers)
+	db.InsertLeagues(source.GetAllLeaguesFromSource())
 
 	router := mux.NewRouter()
-	router.HandleFunc("/league-offers", bethttp.GetLeagueOffers).Methods("GET")
-	router.HandleFunc("/offer/{id}", bethttp.GetOffer).Methods("GET")
+	router.HandleFunc("/league-offers", hdl.GetLeagueOffers).Methods("GET")
+	router.HandleFunc("/offer/{id}", hdl.GetOffer).Methods("GET")
 	log.Fatal(http.ListenAndServe("localhost:5000", router))
-}
-
-type bettingDB struct {
-	conn *sql.DB
-}
-
-func NewDB() *bettingDB {
-	return &bettingDB{
-		conn: mysql.ConnectDB(),
-	}
-}
-
-type handler struct {
-	db *bettingDB
-}
-
-func NewHandler(d *bettingDB) *handler {
-	return &handler{
-		db: d,
-	}
-}
-
-func GetAllLeaguesFromHTTP() *mysql.LeaguesData {
-	url := "https://minus5-dev-test.s3.eu-central-1.amazonaws.com/lige.json"
-	var leagues *mysql.LeaguesData
-	err := bethttp.GetJson(url, &leagues)
-	if err != nil {
-		log.Fatalf("impossible to get leagues from http: %s", err)
-	} else {
-		fmt.Printf("Leagues from web have been reached\n")
-	}
-
-	mysql.NewDB().InsertLeagues(leagues)
-	return leagues
-}
-
-func GetAllOffersFromHTTP() {
-	url := "https://minus5-dev-test.s3.eu-central-1.amazonaws.com/ponude.json"
-	var offers []mysql.Offer
-	err := bethttp.GetJson(url, &offers)
-	if err != nil {
-		log.Fatalf("impossible to get offers from http: %s", err)
-	} else {
-		fmt.Printf("Offers from web have been reached\n")
-	}
-	mysql.NewDB().InsertOffers(offers)
-	mysql.NewDB().InsertTips(offers)
 }
